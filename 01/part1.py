22bb559aa9aee32b43ecfbfb80dca3dcc0a32339
Ulf Wallin <herr.wallin@gmail.com>
2022-12-01 17:21:26 +0100

    Day 1

---
+++ 01/part1.py
@@ -0,0 +1,15 @@
+sum = 0
+maxSum = 0
+with open('input.txt') as file:
+    for line in file:
+        if line.strip() == '':
+            print(sum)
+            # Sum 
+            if sum > maxSum:
+                maxSum = sum
+            sum = 0
+        else:
+            sum += int(line)
+if sum > maxSum:
+    maxSum = sum
+print(maxSum)
---
+++ 01/part2.py
@@ -0,0 +1,20 @@
+sum = 0
+maxSum = 0
+top_three = [0] * 3
+with open('input.txt') as file:
+    for line in file:
+        if line.strip() == '':
+            print(top_three)
+            # Sum 
+            if sum > min(top_three):
+                i = top_three.index(min(top_three))
+                top_three[i] = sum
+            sum = 0
+        else:
+            sum += int(line)
+if sum > min(top_three):
+    print(top_three)
+    i = top_three.index(min(top_three))
+    top_three[i] = sum
+
+print(top_three[0] + top_three[1] + top_three[2])
