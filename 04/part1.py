e501f5ff525ea599bfe4dcd116e23bf5b25abfa5
UlfWallin <herr.wallin@gmail.com>
2022-12-04 07:04:39 +0100

    Day 4

--- 04/part1.py
+++ 04/part1.py
@@ -1,19 +1,15 @@
-import numpy as np
 def secIncludes(b1, e1, b2, e2):
-    if int(b1) >= int(b2) and int(e1) <= int(e2):
+    if int(b1) >= int(b2) and int(e1) <= int(e2) or int(b2) >= int(b1) and int(e2) <= int(e1):
         return True
     return False
 
-pairs = []
 sum = 0
 with open('input.txt') as file:
     for line in file:
-        
         pair = line.strip().split(",")
         r1 = pair[0].split("-")
         r2 = pair[1].split("-")
-        print(r1, r2)
-        if secIncludes(r1[0], r1[1], r2[0], r2[1]) or secIncludes(r2[0], r2[1], r1[0], r1[1]):
+        if secIncludes(r1[0], r1[1], r2[0], r2[1]):
             sum += 1
 
 print(sum)
