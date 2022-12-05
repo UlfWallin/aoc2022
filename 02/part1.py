f70c1339daa8e61ad09e6850cd8fb09b2d0c8e42
Ulf Wallin <herr.wallin@gmail.com>
2022-12-05 09:12:04 +0100

    fix

--- 02/part1.py
+++ 02/part1.py
@@ -4,22 +4,13 @@ with open('input.txt') as file:
     for line in file:
         opponent = line[0]
         me = m[line[2]]
-        print(opponent, me)
         sum += ord(me) - ord('A') + 1
 
         match (me, opponent):
-            case ('A', 'C'):
+            case ('A', 'C') | ('C', 'B') | ('B', 'A'): 
                 sum += 6
-                print('win')
-            case ('C', 'B'):
-                sum += 6
-                print('win')
-            case ('B', 'A'):
-                sum += 6
-                print('win')
-                   
-        if me == opponent:
-            print('draw')
-            sum += 3
+            case _:   
+                if me == opponent:
+                    sum += 3
 
 print(sum)
