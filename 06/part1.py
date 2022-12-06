b9da0a4dcbe080ffd334f2d68f42a345e3ea36e5
Ulf Wallin <herr.wallin@gmail.com>
2022-12-06 08:58:24 +0100

    Minified

--- 06/part1.py
+++ 06/part1.py
@@ -1,14 +1,9 @@
-inp = ""
 with open("input.txt") as file:
     inp = file.readline()
 
 mlen = 4 # change to 14 for part two
-chrs = len(inp)
-for i in range(1, chrs - mlen - 1):
-    b = i
-    e = i + mlen
-    ch = inp[b:e]
-
+for i in range(1, len(inp) - mlen - 1):
+    ch = inp[i:i+mlen]
     if len(set(ch)) == mlen:
         print(ch, i + mlen)
         break
