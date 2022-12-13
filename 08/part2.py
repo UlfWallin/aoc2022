8db9edc8ee70a8bf0ff179ec912503e12ec9ba70
UlfWallin <herr.wallin@gmail.com>
2022-12-13 19:09:20 +0100

    Day 8 - two

--- 08/part2.py
+++ 08/part2.py
@@ -3,25 +3,25 @@ from functools import reduce
 forest = []
 scenic_score = []
 
-def get_view(center: int, height: int, arr, inc):
+def get_view(center: int, height: int, arr, inc: int):
     if center == 0 or center == len(arr) - 1:
         return(0)
-    
     idx = center + inc
     view = 1
-    while arr[idx] < height: # and idx >= 0 and idx < len(arr):
-        view += 1
-        idx += inc
-        if idx < 0 or idx >= len(arr):
+    while height > arr[idx]:
+        if idx == 0 or idx == len(arr) -1:
             break
+        idx += inc
+        view += 1
+
     return(view)
 
-u = get_view(3, 5, [3,5,3,5,3], -1)
 l = get_view(2, 5, [3,3,5,4,9], -1)
+u = get_view(3, 5, [3,5,3,5,3], -1)
 d = get_view(3, 5, [3,5,3,5,3], 1)
 r = get_view(2, 5, [3,3,5,4,9], 1)
 print(u,l,d,r)
-with open("sample.txt") as file:
+with open("input.txt") as file:
     for line in file:
         forest.append(list(map(int,line.strip())))
 
