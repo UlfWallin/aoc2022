c4d25fa1807e2c19cf27a61a8c30252e0414a143
Ulf Wallin <herr.wallin@gmail.com>
2022-12-13 12:56:17 +0100

    Day 10 - two

--- 10/part2.py
+++ 10/part2.py
@@ -1,6 +1,5 @@
 from collections import deque
 
-sig_s = []
 instructions = deque([])
 with open('input.txt') as file:
     for line in file:
@@ -12,7 +11,16 @@ call_stack = deque([])
 instr = instructions.popleft()
 call_stack.append({'instruction': instr, 'cycle': 2 if instr.startswith('addx') else 1})
 
+curr_row = ''
+
 while len(call_stack) > 0:
+    sprite_pos = x_reg + 1
+    row_pos = cycle % 40
+    if row_pos >= sprite_pos - 1 and row_pos <= sprite_pos + 1:
+        curr_row += '#'
+    else:
+        curr_row += '.'
+
     call_stack[0]['cycle'] -= 1
     if call_stack[0]['cycle'] == 0:
         exec = call_stack.popleft()
@@ -25,9 +33,8 @@ while len(call_stack) > 0:
         if len(instructions) > 0:        
             instr = instructions.popleft()
             call_stack.append({'instruction': instr, 'cycle': 2 if instr.startswith('addx') else 1})
+    if cycle % 40 == 0:
+        print(curr_row)
+        curr_row = ''
+        
     cycle += 1
-    if cycle == 20 or (cycle + 20) % 40 == 0:
-        sig_s.append(cycle * x_reg)
-        print(cycle, cycle * x_reg)
-    
-print(sum(sig_s))
